f276fa2e85eaa78e4e1a944140170ff21ec191eb
Raul Quintero <raul.quintero.cursoinfotec@gmail.com>
2024-06-26 09:01:07 -0700

    correcciones

--- src/app/contexts/backoffice/menus/taller/services/taller-servicios-list/taller-servicios-list.component.ts
+++ src/app/contexts/backoffice/menus/taller/services/taller-servicios-list/taller-servicios-list.component.ts
@@ -16,8 +16,8 @@ export class TallerServiciosListComponent {
 
   title: string = "Taller";
   subTitle: string = "Servicios";
-  buttons = [{text: "Agregar", path: "/admin/recepcion/servicios-form/0'"},];
-  pathService="/admin/recepcion/servicios-view";
+  buttons = [{ text: "Agregar", path: "/admin/recepcion/servicios-form/0'" },];
+  pathService = "/admin/recepcion/servicios-view";
   services: any = [];
 
   currentPage: number = 1;
@@ -34,14 +34,14 @@ export class TallerServiciosListComponent {
 
     this.getAllStatusByDpt()
 
-    this.activatedRoute.params.subscribe(({status}) => {
-      console.log('status',status);
-      if(status == 'todos'){
-        this.loadServices(this.currentPage )
-      }else{
+    this.activatedRoute.params.subscribe(({ status }) => {
+      console.log('status', status);
+      if (status == 'todos') {
+        this.loadServices(this.currentPage)
+      } else {
         this.getByStatus(status)
       }
-  });
+    });
 
 
   }
@@ -55,40 +55,41 @@ export class TallerServiciosListComponent {
     //     this.totalPages = data.totalPages;
     //     this.currentPage = data.number + 1;
     //   });
-    this.servicesService.getPaginatedRecepcionData(1).subscribe({
-      next: resp=>{
+    this.servicesService.getPaginatedRecepcionData(2).subscribe({
+      next: resp => {
         this.services = resp;
         this.isLoading = false;
       },
-      error: resp=>{
+      error: resp => {
         this.isLoading = false;
       }
     });
   }
 
   onPageChange(page: number) {
-    this.loadServices(page-1);
+    this.loadServices(page - 1);
 
   }
 
-  getByStatus(status: string){
+  getByStatus(status: string) {
+    
     this.servicesService.getByStatus(status).subscribe({
-      next: resp=>{
+      next: resp => {
         console.log('tallerFilter', resp);
         this.services = resp;
       }
     });
   }
 
-  getAllStatusByDpt(){
+  getAllStatusByDpt() {
     const TALLER = 2;
     this.departamentoService.getEstatusById(TALLER).subscribe({
-      next: resp=>{
+      next: resp => {
         this.estatus = resp;
-        console.log('status',resp);
+        console.log('status', resp);
 
       }
     })
-    this.loadServices(this.currentPage-1);
+    this.loadServices(this.currentPage - 1);
   }
 }
